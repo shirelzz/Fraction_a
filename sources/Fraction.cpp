790de405275942ddd2e0ef20a07de2170da2355b
shirelzz <shirel.zz2016@gmail.com>
2023-04-20 01:06:45 +0300

    add all operators with empty implementations

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include<iostream>
 #include <stdexcept>
+
 using namespace std;
 
 ostream& operator << (ostream& out, const Fraction& frac)
@@ -10,19 +11,23 @@ ostream& operator << (ostream& out, const Fraction& frac)
 	return out;
 }
 
-Fraction::Fraction(int numerator = 0, int denominator = 1) {
+Fraction::Fraction(int numerator , int denominator) {
+
 	if (denominator == 0) {
 		throw runtime_error("Denominator cannot be zero.");
 	}
+
 	int gcd = get_gcd(abs(numerator), abs(denominator));
 	this->numerator = numerator / gcd;
 	this->denominator = denominator / gcd;
+
 	if (this->denominator < 0) {
 		this->numerator *= -1;
 		this->denominator *= -1;
 	}
 }
 int Fraction::get_numerator() const { return numerator; }
+
 int Fraction::get_denominator() const { return denominator; }
 
 
@@ -32,3 +37,129 @@ int Fraction::get_gcd(int a, int b) const {
 	}
 	return get_gcd(b, a % b);
 }
+
+// The + operator to add two fractions and return their sum as another fraction in reduced form.
+Fraction Fraction:: operator+(Fraction const& obj) const{
+	Fraction a(5,3);
+	return a;
+}
+
+Fraction Fraction:: operator+(float const& obj) const{
+	Fraction a(5,3);
+	return a;
+}
+
+
+// The - operator to subtract two fractions and return their difference as another fraction in reduced form.
+	Fraction Fraction::  operator-(Fraction const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	Fraction Fraction::  operator-(float const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	//* The * operator to multiply two fractions and return their product as another fraction in reduced form.
+	Fraction Fraction::  operator*(Fraction const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	Fraction Fraction::  operator*(float const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	// The / operator to divide two fractions and return their quotient as another fraction in reduced form.
+	Fraction Fraction::  operator/(Fraction const& obj) const{
+		Fraction a(5,3);
+		return a;}
+
+	Fraction Fraction::  operator/(float const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	// The == operator to compare two fractions for equality and return true or false.
+	bool Fraction::  operator==(Fraction const& obj) const{
+		bool a = true;
+		return a;	
+	}
+
+	bool Fraction::  operator==(float const& obj) const{
+		bool a = true;
+		return a;
+	}
+
+
+	// All comparison operations (>,<,>=,<=)
+	bool Fraction::  operator>(Fraction const& obj) const{
+		bool a = true;
+		return a;
+	}
+	bool Fraction::  operator>(float const& obj) const{
+		bool a = true;
+		return a;
+	}
+
+	bool Fraction::  operator<(Fraction const& obj) const{
+		bool a = true;
+		return a;
+	}
+	bool Fraction::  operator<(float const& obj) const{
+		bool a = true;
+		return a;
+	}
+
+	bool Fraction::  operator>=(Fraction const& obj) const{
+		bool a = true;
+		return a;
+	}
+	bool Fraction::  operator>=(float const& obj) const{
+		bool a = true;
+		return a;
+	}
+
+	bool Fraction::  operator<=(Fraction const& obj) const{
+		bool a = true;
+		return a;
+	}
+	bool Fraction::  operator<=(float const& obj) const{
+		bool a = true;
+		return a;
+	}
+
+	// The ++ and -- operator that adds (or substracts) 1 to the fraction. implement both pre and post fix.
+	Fraction Fraction::  operator++() const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	Fraction Fraction::  operator--() const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	// The << operator to print a fraction to an output stream in the format “numerator/denominator”.
+	Fraction Fraction::  operator<<(Fraction const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	Fraction Fraction::  operator<<(float const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	// The >> operator to read a fraction from an input stream by taking two integers as input. 
+	Fraction Fraction::  operator>>(Fraction const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
+
+	Fraction Fraction::  operator>>(float const& obj) const{
+		Fraction a(5,3);
+		return a;
+	}
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,19 +1,75 @@
-#pragma once
+#ifndef FRACTION_HPP
+#define FRACTION_HPP
+
 #include<iostream>
 #include <stdexcept>
 using namespace std;
+namespace ariel{}
+
 class Fraction {
+
 private:
 	int numerator;
 	int denominator;
+
 public:
-	Fraction(int, int);
+
+	Fraction(int numerator , int denominator );
+
 	int get_numerator() const;
+
 	int get_denominator() const;
 
 	friend ostream& operator<<(ostream&, const Fraction&);
 
 	int get_gcd(int a, int b) const;
+
+	// The + operator to add two fractions and return their sum as another fraction in reduced form.
+	Fraction operator+(Fraction const& obj) const;
+	Fraction operator+(float const& obj) const;
+
+	// The - operator to subtract two fractions and return their difference as another fraction in reduced form.
+	Fraction operator-(Fraction const& obj) const;
+	Fraction operator-(float const& obj) const;
+
+	//* The * operator to multiply two fractions and return their product as another fraction in reduced form.
+	Fraction operator*(Fraction const& obj) const;
+	Fraction operator*(float const& obj) const;
+
+	// The / operator to divide two fractions and return their quotient as another fraction in reduced form.
+	Fraction operator/(Fraction const& obj) const;
+	Fraction operator/(float const& obj) const;
+
+	// The == operator to compare two fractions for equality and return true or false.
+	bool operator==(Fraction const& obj) const;
+	bool operator==(float const& obj) const;
+
+	// All comparison operations (>,<,>=,<=)
+	bool operator>(Fraction const& obj) const;
+	bool operator>(float const& obj) const;
+
+	bool operator<(Fraction const& obj) const;
+	bool operator<(float const& obj) const;
+
+	bool operator>=(Fraction const& obj) const;
+	bool operator>=(float const& obj) const;
+
+	bool operator<=(Fraction const& obj) const;
+	bool operator<=(float const& obj) const;
+
+	// The ++ and -- operator that adds (or substracts) 1 to the fraction. implement both pre and post fix.
+	Fraction operator++() const;
+
+	Fraction operator--() const;
+
+	// The << operator to print a fraction to an output stream in the format “numerator/denominator”.
+	Fraction operator<<(Fraction const& obj) const;
+	Fraction operator<<(float const& obj) const;
+
+	// The >> operator to read a fraction from an input stream by taking two integers as input. 
+	Fraction operator>>(Fraction const& obj) const;
+	Fraction operator>>(float const& obj) const;
+
+
 };
-
-
+#endif
